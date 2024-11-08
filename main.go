cfd912f82fcb757ebd5d6be669259c3cec9faec8
shanmugapriyan98 <sravic23@asu.edu>
2024-11-07 17:11:57 -0700

    Added dummy handlers for process receipt and get points endpoints

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fetch-receipt-processor/internal/handler"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	router := gin.Default()
+	router.POST("/receipts/process", handler.ProcessReceipt)
+	router.GET("/receipts/:id/points", handler.GetPoints)
 	router.Run("localhost:8080")
 }
